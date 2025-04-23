d73a3c1c771ef5e2d405f0089941942ca087734f
Chiheb <chihababiza9@gmail.com>
2025-04-23 21:36:39 +0100

    create a custom hook called useFetch

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
+import './css/blogs.css';
+import BlogList from "./BlogList";
+import useFetch from './useFetch';
+
 const Home = () => {
+
+    const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
+
     return (
         <div className="home">
-            <h1>HomePage</h1>
+            {isPending && <div>Loading...</div>}
+            {error && <h2>{error}</h2>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
 }
