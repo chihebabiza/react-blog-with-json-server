b7e4872f73244c61c22e2bfb558798a99edc297b
Chiheb <chihababiza9@gmail.com>
2025-04-25 12:22:24 +0100

    update

--- src/useFetch.js
+++ src/useFetch.js
@@ -8,7 +8,6 @@ const useFetch = (url) => {
     useEffect(() => {
 
         const abortCont = new AbortController();
-        setTimeout(() => {
             fetch(url, { signal: abortCont.signal })
                 .then(res => {
                     if (!res.ok) {
@@ -29,7 +28,6 @@ const useFetch = (url) => {
                         setIsPending(false);
                     }
                 })
-        }, 1000);
 
         return () => abortCont.abort();
     }, [url]);
